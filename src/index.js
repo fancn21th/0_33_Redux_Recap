b8797cec676d68cb208f3c90985916bf02ab5ebd
alex.fan <alex.fan@okchem.com>
2017-12-25 15:43:58 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+ReactDOM.render(
+    <div>
+        Hell Redux !
+    </div>,
+    document.getElementById('root')
+);
