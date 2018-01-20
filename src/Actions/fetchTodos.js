a37b6f2b49e0d895421047c4946db3845f61202b
alex.fan <alex.fan@okchem.com>
2018-01-20 15:48:39 +0800

    add more comments about thunk

--- src/Actions/fetchTodos.js
+++ src/Actions/fetchTodos.js
@@ -11,6 +11,12 @@ const receiveTodos = (filter, response) => ({
 // normally a action is a plain object
 // then it could be a promise supported by middleware like redux-promise
 // and then it could be a function by middleware like redux-thunk
+
+// Redux Thunk middleware allows you to write action creators that return a function instead of an action.
+// The thunk can be used to delay the dispatch of an action,
+// or to dispatch only if a certain condition is met.
+// The inner function receives the store methods dispatch and getState as parameters.
+
 // the function take dispatch as well as getState as arguments
 // A thunk is a function that wraps an expression to delay its evaluation.
 
